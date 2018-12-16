55bd8cb0f9417079779a4603bf689cafa6008962
xin jie <xinjie@aptsys.com.sg>
2018-12-16 14:50:21 +0800

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by xin jie on 16/12/18.
+//  Copyright © 2018 xinjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
